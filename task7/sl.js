940d1c04bb9dd77700cef8c6a62c0146af1d86a0
s0177319 <144817835+s0177319@users.noreply.github.com>
2023-10-29 17:27:13 +0300

    Update sl.js

--- task7/sl.js
+++ task7/sl.js
@@ -1,24 +1,18 @@
 $(document).ready(function () {
-    // Инициализация слайдера
-    $('.slider').slick({
-      infinite: false, // Отключает бесконечную прокрутку
-      slidesToShow: 3, // Количество отображаемых слайдов на десктопе
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 981, // Адаптив для смартфонов
-          settings: {
-            slidesToShow: 1,
-          },
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    infinite: false, 
+    slidesToShow: 3, 
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 981, 
+        settings: {
+          slidesToShow: 1,
         },
-      ],
-    });
-  
-    // Обновление информации о страницах при смене слайда
-    $('.slider').on('afterChange', function (event, slick, currentSlide) {
-      var currentPage = currentSlide + 1;
-      var totalPages = slick.slideCount;
-      $('.current-page').text(currentPage);
-      $('.total-pages').text(totalPages);
-    });
+      },
+    ],
   });
+
+});
